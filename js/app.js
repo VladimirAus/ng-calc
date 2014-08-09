b7ea8760721b0fde707345ed62a581454916d683
Vladimir Roudakov <v_rouakov@yahoo.com>
2014-08-10 00:53:06 +1000

    Inital calculator. Working: int numbers, addition, substitution

---
+++ js/app.js
@@ -0,0 +1,36 @@
+angular.module('tes.xperiment.calc', [])
+	.controller('CalcController', ['$scope', function($scope) {
+		$scope.displayNumber = 0;
+		$scope.mem1 = 0;
+		$scope.mem2 = 0;
+		$scope.multiplier = 10;
+		$scope.action = '';
+
+		$scope.actionEq = function() {
+			$scope.actionPlus(1); // TODO: replace with multiple actions
+			$scope.postCmd();
+		};
+		$scope.postCmd = function() {
+			$scope.mem1 = 0;
+			$scope.displayNumber = $scope.mem2;
+			$scope.multiplier = 10;
+		};
+		$scope.actionCe = function() {
+			$scope.displayNumber = 0;
+			$scope.mem1 = 0;
+			$scope.mem2 = 0;
+			$scope.multiplier = 10;
+		};
+		$scope.actionPlus = function(sign) {
+			$scope.mem1 = parseFloat("" + $scope.displayNumber);
+			$scope.mem2 = $scope.mem1 + $scope.mem2*sign;
+			$scope.postCmd();
+		};
+		$scope.clickNum = function(num) {
+			$scope.mem1 = $scope.mem1*$scope.multiplier + num;
+			$scope.displayNumber = $scope.mem1;
+		};
+		$scope.clickDot = function() {
+			$scope.multiplier = 0.1;
+		};
+}]);
